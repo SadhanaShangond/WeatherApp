27b681655a19b660ce085c6566c7a38d9d89b93d
sadhana <sadhana903545@gmail.com>
2025-05-02 19:31:29 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import wsaLogo from "../assets/images/wsa-logo.svg"
+
+export default function Header
+() {
+  return (
+    <div className="header">
+      <img src={wsaLogo} alt="wsa logo" width={183} height={63}></img>
+      <p className="header-text">WEATHER</p>
+    </div>
+  );
+}
